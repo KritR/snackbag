383d9231e0481e08a0533adc02dd241ca7fd400f
Krithik Rao <krdevmail@gmail.com>
2023-11-07 16:08:17 -0300

    Added basic page layout for image conversion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/blocks.css/dist/blocks.min.css" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,41 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello</div>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className='block w-full fixed wrapper block lg'>
+        <div className='p-4'>
+        <h1 className='text-xl my-1'>Image Converter</h1>
+        <p className='font-medium'>Quickly convert between image formats such as jpeg, png, etc.</p>
+        </div>
+      </div>
+      <div className="flex w-full fixed wrapper block lg">
+        <div className="p-4 flex flex-col">
+          <div className="flex flex-row">
+            <button className="block accent">Select Files</button>
+            <button className="block">Clear Files</button>
+          </div>
+
+          <div className="p-4">
+            <p>No files uploaded.</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full fixed wrapper block lg">
+        <div className="p-4 flex flex-col">
+          <h3>Output Format</h3>
+          <select className="block fixed" name="cars" id="cars">
+            <option value="volvo">PNG</option>
+            <option value="saab">JPG</option>
+            <option value="mercedes">TIFF</option>
+            <option value="audi">WEBP</option>
+          </select>
+          <h3>Output Quality %</h3>
+          <input type='number' min='1' max='100' className="block fixed" />
+        </div>
+      </div>
+      <div className="block">
+        Convert and Download
+      </div>
       <div onClick={async () => {
         const blob = await fileOpen({
           mimeTypes: ['image/*'],
