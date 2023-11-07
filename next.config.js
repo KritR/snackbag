54c32fef314df0701773441966358aeaa92d4977
Krithik Rao <krdevmail@gmail.com>
2023-11-06 21:30:49 -0300

    Fixed next export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  webpack(config) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+    return config
+  }
+}
 
 module.exports = nextConfig
