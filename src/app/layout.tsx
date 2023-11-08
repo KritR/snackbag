1fa998e649aecbe661667e4d2859d6ec6968c74e
Krithik Rao <krdevmail@gmail.com>
2023-11-08 18:14:14 -0300

    Added umami tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://unpkg.com/blocks.css/dist/blocks.min.css" />
+        <Script src="https://kritr-umami.fly.dev/script.js" strategy='lazyOnload' data-website-id="5740d187-6a84-4c20-944c-d0fce9aa73c3"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
